e73aa77bfd94364c4d4aa7f3836ed4a849a5ca81
Stuart Mathews <stuart.mathews@finbourne.com>
2019-08-08 10:06:33 +0100

    Add commendary to tutorial01

--- Tutorial01/Box.cs
+++ Tutorial01/Box.cs
@@ -32,26 +32,30 @@ namespace Tutorial01
         }
         
         public bool IsEmpty = true;
-
-        
     }
 
     public static class BoxMethods
     {
+        /// <summary>
+        /// Transforms the contents of a Box in a user defined way
+        /// </summary>
+        /// <typeparam name="TA">The tye of the thing in the box to start with</typeparam>
+        /// <typeparam name="TB">The result type that the transforming function to transform to</typeparam>
+        /// <param name="box">The Box that the extension method will work on</param>
+        /// <param name="map">User defined way to transform the contents of the box</param>
+        /// <returns>The results of the transformation, put back into a box</returns>
         public static Box<TB> Select<TA, TB>(this Box<TA> box, Func<TA, TB> map)
         {
-            // Validate/Check if map should be run
+            // Validate/Check if box is valid(not empty) and if so, run the transformation function on it, otherwise don't
             if (box.IsEmpty)
             {
                 // No, return the empty box
                 return new Box<TB>();
             }
             
-            // Extract
+            // Extract the item from the Box and run the provided transform function ie run the map() function - map is the name of the transformation function the user provided.
             TB transformedItem = map(box.Item);
 
-            
-            // Map
             return new Box<TB>(transformedItem);
         }
     }
--- Tutorial01/Program.cs
+++ Tutorial01/Program.cs
@@ -6,14 +6,23 @@ namespace Tutorial01
     {
         static void Main(string[] args)
         {
+            // A Box, can hold any type but in this case it holds and makes provision for an int. 
+            // Look at the implementation of the Box, notice its just a C# Class I've created.
             Box<int> myNumberBox1 = new Box<int>(1);
            
+            // I can look into the Box
             Console.WriteLine($"The contents of my NumberBox is initially is '{myNumberBox1.Item}'");
 
+            // But this Box is different. It can be used in a Linq Expression. 
+            // This is because it has a special function defined for it call Select(). Have a look at Box class again, check out the extension method at the bottom.
+            // This single function allows the following Linq usages
             var result = from number1 in myNumberBox1
-                select number1 + 1;
+                select number1 + 1; // This is call the Linq Expression Syntax
 
-            Box<int> result2 = myNumberBox1.Select(x => x + 1); // x=> x+1 is the mapping function
+            // This is called the Linq Fluent syntax
+            Box<int> result2 = myNumberBox1.Select(x => x + 1); // x=> x+1 is the transformation function also known as a mapping function
+
+            // Have a Look at Box class again, and specifically the Select() extension method again and try and understand what its doing.
 
             Console.WriteLine($"The number is now after having passed a mapping function1 to it results in its contents being '{result.Item}'");
             Console.WriteLine($"The number is now after having passed a mapping function2 to it results in its contents being '{result2.Item}'");
